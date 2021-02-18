2d26d27a3adc2facbf4643418dbadba8f4a38f92
Kunal Bandooni <57495457+kunalbandooni@users.noreply.github.com>
2021-02-19 00:56:12 +0530

    Updated Method 2

--- code.cpp
+++ code.cpp
@@ -17,8 +17,6 @@ block of height 4 is 3 units and above
 block of height 0 is 7 units. So, the 
 total unit of water trapped is 10 units.
 
-
-
 First try yourself, if unable to understand the logic, the below video might help:
   https://www.youtube.com/watch?v=pq7Xon_VXeU&feature=youtu.be
   
@@ -44,3 +42,36 @@ int trappingWater(int a[], int n){
     }
     return sum;
 }
+//Method 2:-
+//Accepted in LeetCodes
+class Solution{
+    public:
+    int trap(vector<int>& v) {
+        int l=0;
+        int r=v.size()-1;
+        int sum=0;
+        int lmax=0;
+        int rmax=0;
+        while(l<r)
+        {
+            if(v[l]<v[r])
+            {
+                if(v[l]<lmax)
+                    sum+=(lmax-v[l]);
+                else
+                    lmax=v[l];
+                l++;
+            }
+            else
+            {
+                if(v[r]<rmax)
+                    sum+=(rmax-v[r]);
+                else
+                    rmax=v[r];
+                r--;
+                
+            }
+        }
+        return sum;
+    }
+};
